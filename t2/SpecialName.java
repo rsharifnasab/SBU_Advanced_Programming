57892a2004c273b2e7835675e7ac8e065bc4c5bc
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-02-22 00:17:04 +0330

    SpecialName is 2/100

--- t2/SpecialName.java
+++ t2/SpecialName.java
@@ -1,26 +1,39 @@
-public class Jabeja {
+class SpecialName {
+
+
+    public static void cleaner(StringBuilder s) {
+       for (int i=0;i<s.length();i++) {
+         char c = s.charAt(i);
+         if ( c >= 'a' && c <= 'z' ) continue;
+         if ( c >= 'A' && c <= 'Z' ) continue;
+         if ( c=='!' || c=='.' || c==' ') continue;
+         if ( c=='-' || c=='_' || c==' ') continue;
+         s.deleteCharAt(i);
+         i--;
+       }
+    }
 
     public static void main(String[] args) {
 
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        StringBuilder s = new StringBuilder(sc.next());
-        String u = sc.next();
-        String v = sc.next();
-        int v_aval = s.indexOf(v);
-        int u_last = s.lastIndexOf(u);
-        if (v_aval != -1 && u_last != -1) { //none of the above
-            s.replace(v_aval, (v_aval + v.length()) , u);
-        // nedd TODO work!
-        }
-        // doesnt need if,else
-        if (v_aval == -1 && u_last != -1) { //just v
-            s.replace(u_last, u_last + u.length(), v);
+        StringBuilder s = new StringBuilder(sc.nextLine());
+        cleaner(s);
+        //System.out.println(s);
+        java.util.Scanner inp= new java.util.Scanner(s.toString());
+
+        String pre,t = inp.next();
+        while(inp.hasNext()){
+          pre = t;
+          t = inp.next();
+          char last = pre.charAt(pre.length()-1);;
+
+          if(t.length()<3) continue;
+          if(Character.isLowerCase(t.charAt(0))) continue;
+          if(last == '.' || last == '!') continue;
+          //System.err.print("last="+pre + " : ");
+
+          System.out.print(t+ "\n");
         }
 
-        if (u_last == -1 && v_aval != -1) { //just v
-            s.replace(v_aval, v_aval + v.length(), u);
-        }
-        // if none of the above : s is main s!
-        System.out.println(s);
     }
 }
