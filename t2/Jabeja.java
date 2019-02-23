33edb8e6d86d43d0b0313cf5ebfc8226bfd9d794
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-02-23 23:56:30 +0330

    SpecialName is now complete

--- t2/Jabeja.java
+++ t2/Jabeja.java
@@ -8,16 +8,19 @@ public class Jabeja {
         String v = sc.next();
         int v_aval = s.indexOf(v);
         int u_last = s.lastIndexOf(u);
-        if (v_aval != -1 && u_last != -1) { //none of the above
+        if (v_aval != -1 && u_last != -1) {
             s.replace(v_aval, (v_aval + v.length()) , u);
+            //s.replace(u_last, (u_last + u.length()) , v);
+            s.replace(u_last + (u.length() - v.length()), u_last + (u.length() - v.length()) + u.length(), v);
+
         // nedd TODO work!
         }
-        // doesnt need if,else
-        if (v_aval == -1 && u_last != -1) { //just v
+
+        if (v_aval == -1 && u_last != -1) {
             s.replace(u_last, u_last + u.length(), v);
         }
 
-        if (u_last == -1 && v_aval != -1) { //just v
+        if (u_last == -1 && v_aval != -1) {
             s.replace(v_aval, v_aval + v.length(), u);
         }
         // if none of the above : s is main s!
---
+++ t2/magic2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+double eval(string expr)
+{
+    string xxx; // Get Rid of Spaces
+    for (int i = 0; i < expr.length(); i++)
+    {
+        if (expr[i] != ' ')
+        {
+            xxx += expr[i];
+        }
+    }
+
+    string tok = ""; // Do parantheses first
+    for (int i = 0; i < xxx.length(); i++)
+    {
+        if (xxx[i] == '(')
+        {
+            int iter = 1;
+            string token;
+            i++;
+            while (true)
+            {
+                if (xxx[i] == '(')
+                {
+                    iter++;
+                } else if (xxx[i] == ')')
+                {
+                    iter--;
+                    if (iter == 0)
+                    {
+                        i++;
+                        break;
+                    }
+                }
+                token += xxx[i];
+                i++;
+            }
+            //cout << "(" << token << ")" << " == " << to_string(eval(token)) <<  endl;
+            tok += to_string(eval(token));
+        }
+        tok += xxx[i];
+    }
+
+    for (int i = 0; i < tok.length(); i++)
+    {
+        if (tok[i] == '+')
+        {
+            //cout << tok.substr(0, i) + " + " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) + eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) + eval(tok.substr(i+1, tok.length()-i-1));
+        } else if (tok[i] == '-')
+        {
+            //cout << tok.substr(0, i) + " - " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) - eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) - eval(tok.substr(i+1, tok.length()-i-1));
+        }
+    }
+
+    for (int i = 0; i < tok.length(); i++)
+    {
+        if (tok[i] == '*')
+        {
+            //cout << tok.substr(0, i) + " * " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) * eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) * eval(tok.substr(i+1, tok.length()-i-1));
+        } else if (tok[i] == '/')
+        {
+            //cout << tok.substr(0, i) + " / " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) / eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) / eval(tok.substr(i+1, tok.length()-i-1));
+        }
+    }
+
+
+    for (int i = 0; i < tok.length(); i++)
+    {
+        if (tok[i] == '^')
+        {
+            //cout << tok.substr(0, i) + " * " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) * eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return pow( eval(tok.substr(0, i)) , eval(tok.substr(i+1, tok.length()-i-1)));
+        }
+    }
+    //double pow(double x, double y);
+
+    //cout << stod(tok.c_str()) << endl;
+    return stod(tok.c_str()); // Return the value...
+}
+
+int main()
+{
+    string s,t = "";
+    //cin >> s;
+    getline(cin,s);
+    for (int i = 0; i < s.size(); i++) {
+      if(s[i] != ' ') t = t + s[i];
+    }
+    cout <<fixed<<setprecision(2)<< eval(t);
+
+}
