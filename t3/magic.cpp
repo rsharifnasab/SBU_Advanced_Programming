7e6a1de98127210ea597913dd46945c6a3228d57
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-02-27 00:52:07 +0330

    magic is 94/100

--- t3/magic.cpp
+++ t3/magic.cpp
@@ -2,19 +2,27 @@
 int main()
 {
   std::string s;
-  int size =0 ;
-  std::cin >> size >> s;
-  int* arr = new int[size];
-  for (int i = 0; i < size; i++)
+  int n =0 ;
+  std::cin >> n >> s;
+  int* arr = new int[n];
+  int sum = 0;
+  for (int i = 0; i < n; i++){
     arr[i] = s[i]-'0' ;
-
-  int max = size -1;
-  int ok = false;
-  for (int i = max; i > 0; i--)
-  {
-    for
-
+    sum+=arr[i];
   }
-  std::cout << ok?"Yes":"No" << std::endl;
+
+
+  int ok = false;
+
+  for (int had =sum-1; had > 0; had--){
+    int tSum = 0;
+    for(int i = 0; i < n ; i++){
+      tSum += arr[i];
+      if(tSum > had) break;
+      if(tSum == had) tSum = 0;
+    }
+    if(tSum == had || tSum == 0) ok = true;
+  }
+  std::cout << (ok?"Yes":"No") << std::endl;
   return 0;
 }
