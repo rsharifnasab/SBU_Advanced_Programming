9ec7c67d80b541e9d8b9940b1233f9547d1704cd
rsharifnasab <rsharifnasab@gmail.com>
2019-02-27 13:20:17 +0330

    all modules are complete, out of bound exception problem

--- t3/Manage.java
+++ t3/Manage.java
@@ -2,7 +2,7 @@ public class Manage{
 
   public static Emplo[] db = new Emplo[300];
   public static int last =0;
-
+  public static java.util.Scanner sc ;
   public static void hire(){
     String name = sc.next();
     String degree = sc.next();
@@ -13,35 +13,76 @@ public class Manage{
   public static void pay(){
     String name = sc.next();
     int ind = name2index(name);
+    System.err.println("paying "+ind);
     db[ind].pay();
   }
 
 
 
-    public static void get(){
-      String name = sc.next();
-      int ind = name2index(name);
-      int quan = sc.nextInt();
-      if(!db[ind].get(quan))
-        System.out.println("NotEnoughMoney");
+  public static void get(){
+    String name = sc.next();
+    int ind = name2index(name);
+    int quan = sc.nextInt();
+    if(!db[ind].get(quan))
+    System.out.println("NotEnoughMoney");
 
+  }
+
+  public static void loan(){
+    String name = sc.next();
+    int ind = name2index(name);
+
+    if(db[ind].loan())
+      System.out.println("accepted");
+    else
+      System.out.println("rejected");
+
+
+  }
+
+
+  public static void special(){
+    String name = sc.next();
+    int ind = name2index(name);
+    db[ind].makeSpecial();
+  }
+
+
+  public static void promote(){
+      for (int i = 0;i < last ;i++ )
+        db[i].promote();
     }
 
-        public static void special(){
-          String name = sc.next();
-          int ind = name2index(name);
-          db[ind].makeSpecial();
-        }
+
+  public static void regress(){
+      for (int i = 0;i < last ;i++ )
+        db[i].regress();
+    }
+
+
+    public static void cermony(){
+        for (int i = 0;i < last ;i++ )
+          db[i].revam();
+      }
+
+
+    public static void report(){
+      String degree = sc.next();
+      for (int i = 0;i < last ;i++ )
+        db[i].report(degree);
+    }
+
 
   public static int name2index(String name){
     for (int i = 0;i < 300 ;  i++ ) {
       if(db[i].getName().equals(name)) return i;
     }
+    return -1; //TODO
   }
 
 
   public static void main(String[] args) {
-    java.util.Scanner sc = new java.util.Scanner(System.in);
+    sc = new java.util.Scanner(System.in);
 
     do{
       String order = sc.next();
@@ -49,11 +90,15 @@ public class Manage{
       if(order.equals("hire") ) hire();
       if(order.equals("pay")) pay();
       if(order.equals("get")) get();
-      if(order.equals("special")) get();
-      //
+      if(order.equals("special")) special();
+      if(order.equals("loan")) loan();
+      if(order.equals("promote")) promote();
+      if(order.equals("regress")) regress();
+      if(order.equals("report")) report();
       //System.out.println(db[0].getMoney());
-      while(!order!.equals("exit"));
-    }
+    }while(sc.hasNext());
+
+ }
 
 
     public static class Emplo{
@@ -61,23 +106,79 @@ public class Manage{
       private static String name;
       private static boolean Special;
       private static int money;
+      private static boolean vam;
+      private static boolean fired;
+      private static boolean retired;
       public Emplo(String name,String degree){
         money = 0;
         Special = false;
+        fired = false;
+        retired = false;
+        vam = false;
         this.name = name;
         this.degree = degree;
+
       }
       public static String getName(){ return name;}
       public int getSalary(){
-        if(degree == "Worker") return 100;
-        if(degree == "Foreman") return 300;
-        if(degree == "Supervisor") return 700;
-        if(degree == "Leader") return 900;
-        return -1;
+        if(degree.equals("Worker")) return 100;
+        if(degree.equals("Foreman")) return 300;
+        if(degree.equals("Supervisor")) return 700;
+        if(degree.equals("Leader")) return 900;
+        return 0;
       }
+
       public void pay(){
         money = money + getSalary();
+        System.err.println("saalry = "+getSalary()+ " deg = "+degree);
       }
+
+      public void promote(){
+        if(money <= 2* getSalary())return;
+        System.err.println("promoting" + getName());
+        if(degree.equals("Worker")) degree = "Foreman";
+        else if(degree.equals("Foreman")) degree = "Supervisor";
+        else if(degree.equals("Supervisor")) degree = "Leader";
+        else if(degree.equals("Leader")) {
+          retired = true;
+          if(Special) cermony();
+        }
+      }
+
+
+
+      public void report(String degree){
+        if (this.degree.equals(degree) == false) return;
+        if(Special) System.out.print("special ");
+        System.out.print(getName()+" ");
+        if(!fired)System.out.print("("+degree+") "); // TODO
+        System.out.print(getMoney()+" ");
+        System.out.println();
+      }
+
+
+      public void regress(){
+        if(money >=  getSalary() / 2 )return; // TODO
+
+        if(degree.equals("Worker")) fired = true;//degree = "Fired";
+        else if(degree.equals("Foreman")) degree = "Worker";
+        else if(degree.equals("Supervisor")) degree = "Foreman";
+        else if(degree.equals( "Leader")) degree = "Supervisor";
+      }
+
+
+      public boolean loan(){
+        if(vam) return false;
+        vam = true;
+        money= money + getSalary()*3;
+        return true;
+      }
+
+
+      public void revam(){
+        if (Special) vam = false;
+      }
+
       public static boolean get(int quan){
         if(money > quan){
           money-=quan;
@@ -87,7 +188,7 @@ public class Manage{
       }
 
       public static boolean special(){ return Special;}
-      public static makeSpecial(){ Special = true;}
+      public static void makeSpecial(){ Special = true;}
       public static int getMoney(){
         return money;
       }
@@ -97,4 +198,4 @@ public class Manage{
 
 
   }
-// still god enough
+  // still god enough
