1a6fb94f6837e2d8277dbdb5983072d43945392e
rsharifnasab <rsharifnasab@gmail.com>
2019-02-27 11:23:20 +0330

    added getmoney and salary

--- t3/Manage.java
+++ t3/Manage.java
@@ -1,24 +1,39 @@
 public class Manage{
 
+  public static Emplo[] db = new Emplo[100];
+  public static int last =0;
   public static void main(String[] args) {
 
+    do{
+      db[last++] = new Emplo("Worker");
+      System.out.println(db[0].getMoney());
+    while(order!="exit");
   }
 
 
-}
+  public static class Emplo{
+    public static String degree;
+    private static String name;
 
-private class emplo{
-  public String degree;
-  private String name;
+    private static int money;
+    public Emplo(String name,String degree){
+      money = 0;
+      this.name = name;
+      this.degree = degree;
+    }
+    public int getSalary(){
+      if(degree == "Worker") return 100;
+      if(degree == "Foreman") return 300;
+      if(degree == "Supervisor") return 700;
+      if(degree == "Leader") return 900;
+      return -1;
+    }
+    public int getMoney(){
+      return money;
+    }
 
-  private int money = 0;
 
-  public int getSalary(){
-    if(degree == "Worker") return 100;
-    if(degree == "Foreman") return 300;
-    if(degree == "Supervisor") return 700;
-    if(degree == "Leader") return 900;
   }
-  
+
 
 }
