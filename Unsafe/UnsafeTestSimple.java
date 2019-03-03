52d685e41fd433fe611e5a48eda46d7793ca1e4b
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-03 22:58:45 +0330

    cleaning the original

--- Unsafe/UnsafeTestSimple.java
+++ Unsafe/UnsafeTestSimple.java
@@ -1,9 +1,8 @@
-
 import java.lang.reflect.Field;
 import java.util.Random;
 import sun.misc.Unsafe;
 
-public class UnsafeTest {
+public class UnsafeTestSimple {
 
 
   private static void danglingMaker() {
