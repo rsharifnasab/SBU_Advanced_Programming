8aa7514c58b9aa7549774f7d8e6f9ad37b5dc575
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-15 17:28:39 +0330

    magic is 100/100

--- t3/magic.cpp
+++ t3/magic.cpp
@@ -3,19 +3,23 @@ int main()
 {
   std::string s;
   int n =0 ;
-  std::cin >> n >> s;
+  std::cin >> n;
+  if(n==0) {
+     std::cout<<"Yes\n";
+     return 0;
+  }
+  std:: cin >> s;
   int* arr = new int[n];
   int sum = 0;
   for (int i = 0; i < n; i++){
     arr[i] = s[i]-'0' ;
     sum+=arr[i];
   }
-
+ if(sum == 0){ std::cout << "Yes\n" ; return 0;}
   const int max = sum -1;
   int ok = false;
 
   for (int had = max-1; had > 0; had--){
-  //  std::cerr << " had is " << had << std::endl;
     int tSum = 0;
     for(int i = 0; i < n ; i++){
       tSum += arr[i];
