3e3ecc7f981dfef347da10cf332a40d228b85a37
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-16 18:08:14 +0330

    converted trip handler to singleton

--- t4/Taxi/source/TripHandler.java
+++ t4/Taxi/source/TripHandler.java
@@ -1,5 +1,12 @@
 
 public class TripHandler {
+	private static TripHandler onlyInstance = new TripHandler();
+
+	public static  TripHandler getInstance(){
+		return onlyInstance;
+	}
+
+	private TripHandler(){}
 
 	public int calcPrice(String type, TripParam params) {
 		//your implementation
