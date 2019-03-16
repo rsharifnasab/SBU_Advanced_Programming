10c8b7675300f56baee84ec57a97d9dfbddcd0de
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-16 20:35:18 +0330

    removed todo of economic Taxi

--- t4/Taxi/source/TripHandler.java
+++ t4/Taxi/source/TripHandler.java
@@ -9,8 +9,7 @@ public class TripHandler {
 
 	public int calcPrice(String type, TripParam params) {
 			if(type.equals("vip")) return (new VIPTripMethod().calcPrice(params));
-			//if(type.equals("economic")) return (new EconomicTripMethod().calcPrice(params));
-			//TODO
+			if(type.equals("economic")) return (new EconomicTripMethod().calcPrice(params));
 			if(type.equals("bike")) return (new BikeTripMethod().calcPrice(params));
 			return 0;
 	}
