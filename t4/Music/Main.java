86a440cca2a0c1c35b8bdec53f354cf86869f046
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-16 21:59:48 +0330

    added main

---
+++ t4/Music/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] args) {
+    PlayList playList = new PlayList(50);
+    playList.addMusic(new Music("a", "b", "c", 2000));
+    playList.addMusic(new Music("aa", "bb", "cc", 2000));
+    playList.addMusic(new Music("aaa", "b", "cc", 2000));
+    playList.addMusic(new Music("aaa", "b", "ccc", 2010));
+
+    Object[] names = playList.getMusics();
+    System.out.println("length of names : " + names.length);
+    for (int i = 0; i < playList.getNumberOfMusics(); i++) {
+      System.out.println(names[i]);
+}
+
+  }
+}
--- t4/Music/PlayList.java
+++ t4/Music/PlayList.java
@@ -1,6 +1,7 @@
 public class PlayList{
   private Music[] db;
   private int last=0;
+
   public PlayList(int size){
     db = new Music[size];
   }
@@ -10,16 +11,19 @@ public class PlayList{
   }
 
   public int getNumberOfMusics(){
-    return db.size;
+    return last;
   }
 
-  public int isFull(){
-    return(last+1>=db.size);
+  public boolean isFull(){
+    return((last+1)>=db.length);
   }
+
   public boolean addMusic(Music music){
     if(this.isFull()) return false;
     db[last++] = music;
     return true;
   }
+
+
 
 }
