a4f08f5da8b5313854a4720984b3b3b86682504b
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-16 16:37:47 +0330

    A.java added

---
+++ t4/Objective/A.java
@@ -0,0 +1,12 @@
+public class A{
+  public final String method1(){
+    String ans = getClass().getName();
+    ans = ans + "1";
+    return ans;
+  }
+  public String method2(){
+    String ans = getClass().getName();
+    ans = ans + "2";
+    return ans;
+  }
+}
