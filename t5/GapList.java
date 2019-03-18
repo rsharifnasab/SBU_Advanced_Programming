e66a62c4310785841863f24dd96e85d051a54f33
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-18 11:26:23 +0330

    added basic GapList.java still dangling

---
+++ t5/GapList.java
@@ -0,0 +1,34 @@
+public class GapList{
+
+  public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    while (sc.hasNext()) {
+
+    }
+  }
+
+
+/*
+  Arrays.sort(myTypes, MyType::compareThem);
+  public static int compareThem(MyType a, MyType b) {
+    return a.name.compareTo(b.name);
+  }
+
+
+*/
+class Users{
+  private String fName;
+  private String lName;
+  private String Number;
+  private Users[] friends
+  public Users(String fName,String lName, String Number){
+    this.fName = fName;
+    this.lName = lName;
+    this.Number = Number;
+  }
+class Friends extends Users{
+  
+}
+
+
+}
