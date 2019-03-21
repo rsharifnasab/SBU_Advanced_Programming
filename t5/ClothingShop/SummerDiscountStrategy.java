726ef46beccbfa8ae67730fbc789df53cf63ba94
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-21 13:39:20 +0330

    completed summerDiscount

--- t5/ClothingShop/SummerDiscountStrategy.java
+++ t5/ClothingShop/SummerDiscountStrategy.java
@@ -3,6 +3,13 @@ public class SummerDiscountStrategy implements DiscountStrategy
     @Override
     public long priceByDiscount(Clothing clothing)
     {
-        // TODO
+        long basePrice = clothing.getBasePrice();
+        Season season = clothing.getSeason();
+        //SPRING, SUMMER, FALL, WINTER
+        if(season == Season.SPRING) return 0.6*basePrice;
+        if(season == Season.SUMMER) return 0.5*basePrice;
+        if(season == Season.FALL) return 1*basePrice;
+        if(season == Season.WINTER) return 0.7*basePrice;
+
     }
 }
