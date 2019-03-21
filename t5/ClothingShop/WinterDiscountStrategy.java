ceeceb24037fbd48b62ba63b1371629667ae3e73
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-21 13:44:48 +0330

    completed winterDiscount

--- t5/ClothingShop/WinterDiscountStrategy.java
+++ t5/ClothingShop/WinterDiscountStrategy.java
@@ -3,6 +3,19 @@ public class WinterDiscountStrategy implements DiscountStrategy
     @Override
     public long priceByDiscount(Clothing clothing)
     {
-        // TODO
+      long basePrice = clothing.getBasePrice();
+      Season season = clothing.getSeason();
+      //SPRING, SUMMER, FALL, WINTER
+      Long price = basePrice
+      if(season == Season.SPRING) price = 0.6*basePrice;
+      if(season == Season.SUMMER) price = 0.75*basePrice;
+      if(season == Season.WINTER) price = 0.7*basePrice;
+
+
+      String clothType = clothing.getName(); //ERROR?
+      if(clothType.equals("Jacket")) price *= 0.9;
+
+      return price;
+
     }
 }
