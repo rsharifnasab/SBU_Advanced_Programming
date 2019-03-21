baf67072e8dc7e9daabd7f01f3e37d5e91405e95
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-03-22 01:33:59 +0430

    working on getter and setter of shamsi cal

--- t5/Calendar/calendar/ShamsiCustomCalendar.java
+++ t5/Calendar/calendar/ShamsiCustomCalendar.java
@@ -4,5 +4,56 @@ import representation.CalendarRepresentation;
 import exception.IllegalCalendarFormatExceptoin;
 
 class ShamsiCustomCalendar implements CustomCalendar {
+    private int day;
+    private int month;
+    private int year;
+    private static final String[] monthDB = {null,"Farvardin","Ordibehesht","Khordad","Tir","Mordad","Shahrivar","Mehr","Aban","Azar","Dey","Bahman","Esfand"};
+
+    CalendarRepresentation representation;
+
+    int getDay(){
+      return year;
+    }
+
+    void setDay(int day){
+      this.day = day;
+    }
+
+    int getMonth(){
+      return month;
+    }
+
+    void setMonth(int month){
+      this.month = month;
+    }
+
+    int getYear(){
+      return year;
+    }
+
+    void setYear(int year){
+      this.year = year;
+    }
+
+    String getMonthName(){
+      return monthDB[month];
+    }
+
+    CustomCalendar increaseOneDay();
+
+    CustomCalendar decreaseOneDay();
+
+    void setCalendarRepresentation(CalendarRepresentation representation){
+      this.representation = representation;
+    }
+
+    int compareTo(CustomCalendar object){
+      //if(t)
+    }
+
+    String toString(){
+      if(representation == null) throw new IllegalStateException("no representation");
+      representation.getRepresentation(this);
+    }
 
 }
