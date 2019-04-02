b06c4363b5c8bc199c19abb3f57f27c9c46dd329
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-02 23:35:07 +0430

    test.java reverted to original!

--- t5/Calendar/Test.java
+++ t5/Calendar/Test.java
@@ -7,62 +7,62 @@ import representation.FullRepresentation;
 
 public class Test
 {
-  public static void main(String[] args) {
-    CustomCalendar shamsiCalendar = new ShamsiCustomCalendar("1/1/1397");
-    shamsiCalendar.setCalendarRepresentation(new DateRepresentation());
-    System.out.println(shamsiCalendar); // 01/01/1397
-    shamsiCalendar.setCalendarRepresentation(new FullRepresentation());
-    System.out.println(shamsiCalendar); // 1 Farvardin 1397
-    shamsiCalendar.increaseOneDay();
-    System.out.println(shamsiCalendar); // 2 Farvardin 1397
-    shamsiCalendar.decreaseOneDay().decreaseOneDay();
-    System.out.println(shamsiCalendar); // 30 Esfand 1396
-    shamsiCalendar.increaseOneDay();
-    System.out.println(shamsiCalendar); // 1 Farvardin 1397
-    try {
-      shamsiCalendar.setMonth(13);
-      System.out.println("Unable to catch IllegalArgumentException");
-    } catch (IllegalArgumentException e) {
-      System.out.println("Catch IllegalArgumentException");
-    }
-    shamsiCalendar.setDay(31);
-    try {
-      shamsiCalendar.setMonth(7);
-      System.out.println("Unable to catch IllegalArgumentException");
-    } catch (IllegalArgumentException e) {
-      System.out.println("Catch IllegalArgumentException");
-    }
-    shamsiCalendar.setCalendarRepresentation(new DateRepresentation());
-    System.out.println(shamsiCalendar); // 31/01/1397
-    shamsiCalendar.setDay(30);
-    shamsiCalendar.setMonth(12);
-    shamsiCalendar.increaseOneDay().increaseOneDay().increaseOneDay().decreaseOneDay();
-    System.out.println(shamsiCalendar); // 02/01/1398
+    public static void main(String[] args) {
+        CustomCalendar shamsiCalendar = new ShamsiCustomCalendar("1/1/1397");
+        shamsiCalendar.setCalendarRepresentation(new DateRepresentation());
+        System.out.println(shamsiCalendar); // 01/01/1397
+        shamsiCalendar.setCalendarRepresentation(new FullRepresentation());
+        System.out.println(shamsiCalendar); // 1 Farvardin 1397
+        shamsiCalendar.increaseOneDay();
+        System.out.println(shamsiCalendar); // 2 Farvardin 1397
+        shamsiCalendar.decreaseOneDay().decreaseOneDay();
+        System.out.println(shamsiCalendar); // 30 Esfand 1396
+        shamsiCalendar.increaseOneDay();
+        System.out.println(shamsiCalendar); // 1 Farvardin 1397
+        try {
+            shamsiCalendar.setMonth(13);
+            System.out.println("Unable to catch IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Catch IllegalArgumentException");
+        }
+        shamsiCalendar.setDay(31);
+        try {
+            shamsiCalendar.setMonth(7);
+            System.out.println("Unable to catch IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Catch IllegalArgumentException");
+        }
+        shamsiCalendar.setCalendarRepresentation(new DateRepresentation());
+        System.out.println(shamsiCalendar); // 31/01/1397
+        shamsiCalendar.setDay(30);
+        shamsiCalendar.setMonth(12);
+        shamsiCalendar.increaseOneDay().increaseOneDay().increaseOneDay().decreaseOneDay();
+        System.out.println(shamsiCalendar); // 02/01/1398
 
-    try {
-      CustomCalendar gregorianCalendar = new GregorianCustomCalendar("salam");
-      System.out.println("Unable to catch IllegalCalendarFormatException");
-    } catch (IllegalCalendarFormatException e) {
-      System.out.println("Catch IllegalCalendarFormatException");
-    }
-    CustomCalendar gregorianCalendar = new GregorianCustomCalendar("01/1/1980");
-    try {
-      System.out.println(gregorianCalendar); // representation must be set before print
-      System.out.println("Unable to catch IllegalStateException");
-    } catch (IllegalStateException e) {
-      System.out.println("Catch IllegalStateException");
-    }
-    gregorianCalendar.setCalendarRepresentation(new DateRepresentation());
-    System.out.println(gregorianCalendar); // 01/01/1980
-    gregorianCalendar.setYear(2020);
-    gregorianCalendar.setMonth(12);
-    while (gregorianCalendar.getMonth() != 1)
-    gregorianCalendar.increaseOneDay();
-    System.out.println(gregorianCalendar); // 01/01/2021
+        try {
+            CustomCalendar gregorianCalendar = new GregorianCustomCalendar("salam");
+            System.out.println("Unable to catch IllegalCalendarFormatException");
+        } catch (IllegalCalendarFormatException e) {
+            System.out.println("Catch IllegalCalendarFormatException");
+        }
+        CustomCalendar gregorianCalendar = new GregorianCustomCalendar("01/1/1980");
+        try {
+            System.out.println(gregorianCalendar); // representation must be set before print
+            System.out.println("Unable to catch IllegalStateException");
+        } catch (IllegalStateException e) {
+            System.out.println("Catch IllegalStateException");
+        }
+        gregorianCalendar.setCalendarRepresentation(new DateRepresentation());
+        System.out.println(gregorianCalendar); // 01/01/1980
+        gregorianCalendar.setYear(2020);
+        gregorianCalendar.setMonth(12);
+        while (gregorianCalendar.getMonth() != 1)
+            gregorianCalendar.increaseOneDay();
+        System.out.println(gregorianCalendar); // 01/01/2021
 
-    System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("02/01/2021")) < 0); // true
-    System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("01/02/2021")) < 0); // true
-    System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("15/12/2020")) > 0); // true
-    System.out.println(gregorianCalendar.compareTo(gregorianCalendar));
-  }
+        System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("02/01/2021")) < 0); // true
+        System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("01/02/2021")) < 0); // true
+        System.out.println(gregorianCalendar.compareTo(new GregorianCustomCalendar("15/12/2020")) > 0); // true
+        System.out.println(gregorianCalendar.compareTo(gregorianCalendar)); // 0
+    }
 }
