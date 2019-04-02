750189c110b9cd73f07026039ef40355e6ff0202
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-02 21:37:17 +0430

    added simple full represenation and increse day added

--- t5/Calendar/calendar/ShamsiCustomCalendar.java
+++ t5/Calendar/calendar/ShamsiCustomCalendar.java
@@ -4,60 +4,87 @@ import representation.CalendarRepresentation;
 import exception.IllegalCalendarFormatExceptoin;
 
 class ShamsiCustomCalendar implements CustomCalendar {
-    private int day;
-    private int month;
-    private int year;
-    private static final String[] monthDB = {null,"Farvardin","Ordibehesht","Khordad","Tir","Mordad","Shahrivar","Mehr","Aban","Azar","Dey","Bahman","Esfand"};
+  private int day;
+  private int month;
+  private int year;
+  private static final String[] monthDB = {null,"Farvardin","Ordibehesht","Khordad","Tir","Mordad","Shahrivar","Mehr","Aban","Azar","Dey","Bahman","Esfand"};
 
-    CalendarRepresentation representation;
+  CalendarRepresentation representation;
 
-    public int getDay(){
-      return this.day;
+  public int getDay(){
+    return this.day;
+  }
+
+  public void setDay(int day) throws IllegalArgumentException{
+    if(day>31) throw new IllegalArgumentException("date is bigger than 31");
+    if(day<1) throw new IllegalArgumentException("date should be bigger mosavi 1");
+    if(month>6 && (day==31)) throw new IllegalArgumentException("invalid date");
+    this.day = day;
+  }
+
+  public int getMonth(){
+    return month;
+  }
+
+  public void setMonth(int month){
+    if(month > 12) throw new IllegalArgumentException("month is bigger than 12");
+    if(month < 1) throw new IllegalArgumentException("month is lower than 1");
+    this.month = month;
+  }
+
+  public int getYear(){
+    return year;
+  }
+
+  public void setYear(int year){
+    if(year < 1) throw new IllegalArgumentException("year should be bigger mosavi than 1");
+    this.year = year;
+  }
+
+  public String getMonthName(){
+    return monthDB[month];
+  }
+
+  public CustomCalendar increaseOneDay(){
+    fMonth = this.month;
+    fDay = this.day;
+    fYear = this.year;
+    try{
+      try{
+        setDay(fDay+1);
+      }
+      catch(exception e){
+        setDay(1);
+        try{
+          setMonth(fMonth+1);
+        }
+        catch(exception e){
+          setMonth(1);
+          setYear(fYear+1);
+        }
+
+      }
+    }
+    catch(exception e){
+      System.err.println("error!");
+    }
+  }
+
+    public CustomCalendar decreaseOneDay(){
+
     }
 
-    public void setDay(int day) throws IllegalArgumentException{
-      if(day>31) throw new IllegalArgumentException("date is bigger than 31");
-      if(day<1) throw new IllegalArgumentException("date should be bigger mosavi 1");
-      if(month>6 && (day==31)) throw new IllegalArgumentException("invalid date");
-      this.day = day;
-    }
-
-    int getMonth(){
-      return month;
-    }
-
-    void setMonth(int month){
-      
-      this.month = month;
-    }
-
-    int getYear(){
-      return year;
-    }
-
-    void setYear(int year){
-      this.year = year;
-    }
-
-    String getMonthName(){
-      return monthDB[month];
-    }
-
-    CustomCalendar increaseOneDay();
-
-    CustomCalendar decreaseOneDay();
-
-    void setCalendarRepresentation(CalendarRepresentation representation){
+    public void setCalendarRepresentation(CalendarRepresentation representation){
       this.representation = representation;
     }
 
-    int compareTo(CustomCalendar object){
+    public int compareTo(CustomCalendar object){
       //if(t)
     }
 
-    String toString(){
+    public String toString(){
       if(representation == null) throw new IllegalStateException("no representation");
       representation.getRepresentation(this);
     }
 
-}
+  }
---
+++ t5/Calendar/representation/FullRepresentation.java
@@ -0,0 +1,10 @@
+package representation;
+
+import calendar.CustomCalendar;
+import exception.IllegalCalendarFormatExceptoin;
+
+public class FullRepresentation implements CalendarRepresentation {
+    String getRepresentation(CustomCalendar calendar){
+
+    }
+}
