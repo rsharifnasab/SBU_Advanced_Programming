11bdc4b31e9300086a93f1231c53f853269c123a
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-03 20:37:08 +0430

    added join and name2index method

--- t5/GapList.java
+++ t5/GapList.java
@@ -1,34 +1,63 @@
 public class GapList{
+  private static java.util.Scanner sc = new java.util.Scanner(System.in);
+  private static  User [] users = new User[2000];
+  private static int lastUser = 0;
+  private static int name2ind(String fName,String lName){
+    for (int i = 0 ; i < lastUser; i++) {
+      if(users[i].getFName().equals(fName) && users[i].getLName().equals(lName)) return i;
+    }
+    return -1;
+  }
+  private static boolean join(){
+    String fName = sc.next();
+    String lName = sc.next();
+    if(name2ind(fName,lName) != -1 ){ // users exists
+      System.out.println("joinError");
+      return false;
+    }
+    users[lastUser] = new User(fName,lName);
+    while (sc.hasNextInt()) {
+      lastUser.addNum(sc.next());
+    }
+    lastUser++;
+    return true;
+  }
+
 
   public static void main(String[] args) {
-    java.util.Scanner sc = new java.util.Scanner(System.in);
     while (sc.hasNext()) {
+      String order = sc.next();
+      if(order.equals("join")) join();
 
     }
   }
-
+}
 
 /*
   Arrays.sort(myTypes, MyType::compareThem);
   public static int compareThem(MyType a, MyType b) {
     return a.name.compareTo(b.name);
   }
-
-
 */
-class Users{
+class User{
   private String fName;
   private String lName;
-  private String Number;
-  private Users[] friends
-  public Users(String fName,String lName, String Number){
+  private String[] Number = new String[10];
+  private int numberCount = 0;
+  private Friend[] friends = new Friend[1000];
+  private int friendCount = 0;
+  public User(String fName,String lName){
     this.fName = fName;
     this.lName = lName;
-    this.Number = Number;
   }
-class Friends extends Users{
-  
+
 }
 
-
+class Friend{
+  boolean special = false;
+  boolean blocked = false;
+  User contact;
+  Friend(User contact){
+    this.contact = contact;
+  }
 }
