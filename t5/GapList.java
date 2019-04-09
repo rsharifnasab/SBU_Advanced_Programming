cf749e5da79abdc7b61b0d71a981cc864dd71654
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-09 21:47:49 +0430

    working on bug fixes

--- t5/GapList.java
+++ t5/GapList.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
 public class GapList{
-  private static java.util.Scanner sc = new java.util.Scanner(System.in);
+  private static Scanner sc = new Scanner(System.in);
   private static  User [] users = new User[2000];
   private static int lastUser = 0;
 
   private static int name2ind(String fName,String lName){
-    for (int i = 0 ; i < lastUser; i++) {
+    for (int i = 0; i < lastUser; i++) {
       if(users[i].getFName().equalsIgnoreCase(fName) && users[i].getLName().equalsIgnoreCase(lName)) return i;
     }
     return -1;
   }
-
-
 
   private static boolean join(){
     String fName = sc.next();
@@ -20,7 +18,8 @@ public class GapList{
     User temp = new User(fName,lName);
     while (sc.hasNextLong()) {
       String num = sc.next();
-      if(searchByNum(num) != -1){
+      if(searchByNum(num) == -1){ /* ok */ }
+      else{
         System.out.println("joinError");
         return false;
       }
@@ -30,14 +29,12 @@ public class GapList{
       System.out.println("joinError");
       return false;
     }
-
     users[lastUser++] = temp;
-
     return true;
   }
 
   private static int searchByNum(String num){
-    for (int i = 0 ; i < lastUser; i++) {
+    for (int i = 0; i < lastUser; i++) {
       for (String n : users[i].number) {
         if(n==null) continue;
         long l1 = Long.parseLong(n);
@@ -52,7 +49,7 @@ public class GapList{
     String fName = sc.next();
     String lName = sc.next();
     int last = lName.length()-1;
-    if(lName.charAt(last) == ':') lName = lName.substring(0,last);
+    if(lName.charAt(last) == ':') lName = lName.substring(0,last); //TODO
     return name2ind(fName,lName);
   }
 
@@ -79,13 +76,14 @@ public class GapList{
     }
     lastUser--;
     for (int i = 0 ; i < lastUser; i++) {
-      if(users[i] == null) continue;
+      //if(users[i] == null) continue;
       for (int j = 0; j < users[i].friends.length; j++) {
         if(users[i].friends[j] == null) continue;
         if(users[i].friends[j].contact == toDel){
           for (int l = j; l<users[i].friendCount; l++ ) {
             users[i].friends[l] = users[i].friends[l+1]; //TODO-
           }
+          users[i].friendCount--;
         }
       }
     }
@@ -95,10 +93,17 @@ public class GapList{
   private static boolean addContact(){
     int userIndex = search();
     int friendIndex = search();
-    if(userIndex == -1 || friendIndex == -1 || friendIndex == userIndex || users[userIndex].isFriend(users[friendIndex])){
+
+    if(userIndex == -1 || friendIndex == -1 || friendIndex == userIndex){
       System.out.println("addContactError");
       return false;
     }
+
+    if(users[userIndex].isFriend(users[friendIndex])) {
+      System.out.println("addContactError");
+      return false;
+    }
+
     users[userIndex].addFriend(users[friendIndex]);
     return true;
   }
@@ -113,8 +118,6 @@ public class GapList{
     return true;
   }
 
-
-
   public static boolean listContacts(){
     String sortBy = sc.next();
     int userIndex = search();
@@ -127,21 +130,26 @@ public class GapList{
       @Override
       public int compare(Friend o1, Friend o2) {
 
-          if (o1 == null && o2 == null) { return 0; }
+          if (o1 == null && o2 == null) { return 1; }
           if (o1 == null) { return 1; }
           if (o2 == null) { return -1;}
 
+          if(o1.isSpecial() && !o2.isSpecial()) return 1;
+          if(!o1.isSpecial() && o2.isSpecial()) return -1;
+
           if (sortBy.equals("first-name")){
-            if(o1.contact.getFName().equalsIgnoreCase(o2.contact.getFName())) return o1.contact.getLName().compareTo(o2.contact.getLName());
-            return o1.contact.getFName().compareTo(o2.contact.getFName());
+            if(o1.contact.getFName().equalsIgnoreCase(o2.contact.getFName())) return o1.contact.getLName().compareToIgnoreCase(o2.contact.getLName());
+            return o1.contact.getFName().compareToIgnoreCase(o2.contact.getFName());
           }
-          if(o1.contact.getLName().equalsIgnoreCase(o2.contact.getLName())) return o1.contact.getFName().compareTo(o2.contact.getFName());
-          return o1.contact.getLName().compareTo(o2.contact.getLName());
+
+          if(o1.contact.getLName().equalsIgnoreCase(o2.contact.getLName())) return o1.contact.getFName().compareToIgnoreCase(o2.contact.getFName());
+          return o1.contact.getLName().compareToIgnoreCase(o2.contact.getLName());
 
       }});
 
     for (Friend f : users[userIndex].friends) {
       if(f == null) continue;
+      if (f.isBlocked()) continue;
       System.out.println(f.contact);
     }
     return true;
@@ -155,16 +163,17 @@ public class GapList{
     @Override
     public int compare(User o1, User o2) {
 
-        if (o1 == null && o2 == null) { return 0; }
+        if (o1 == null && o2 == null) { return 1; }
         if (o1 == null) { return 1; }
         if (o2 == null) { return -1;}
 
+//compareToIgnoreCase()
         if (sortBy.equals("first-name")){
-          if(o1.getFName().equalsIgnoreCase(o2.getFName())) return o1.getLName().compareTo(o2.getLName());
-          return o1.getFName().compareTo(o2.getFName());
+          if(o1.getFName().equalsIgnoreCase(o2.getFName())) return o1.getLName().compareToIgnoreCase(o2.getLName());
+          return o1.getFName().compareToIgnoreCase(o2.getFName());
         }
-        if(o1.getLName().equalsIgnoreCase(o2.getLName())) return o1.getFName().compareTo(o2.getFName());
-        return o1.getLName().compareTo(o2.getLName());
+        if(o1.getLName().equalsIgnoreCase(o2.getLName())) return o1.getFName().compareToIgnoreCase(o2.getFName());
+        return o1.getLName().compareToIgnoreCase(o2.getLName());
 
     }});
 
@@ -254,12 +263,6 @@ private static boolean unSpecial(){
   }
 }
 
-/*
-  Arrays.sort(myTypes, MyType::compareThem);
-  public static int compareThem(MyType a, MyType b) {
-    return a.name.compareTo(b.name);
-  }
-*/
 class User{
 
   private String fName;
@@ -301,8 +304,6 @@ class User{
     numS = numS.substring(0,goodLen) + "]";
     return fName + " " + lName + " " + numS;
   }
-
-
 
 }
 
