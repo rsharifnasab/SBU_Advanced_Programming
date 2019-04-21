ea1356395a51c81ae300f8a4847526ae17e06396
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-21 10:33:28 +0430

    second changed to polymorphism

--- t6/refactoring/Second.java
+++ t6/refactoring/Second.java
@@ -1,6 +1,10 @@
-// many_imports  all imports changeds to java.util.*
-// bad_name Scanner s changed to myScanner
-// bad_name  inputNumber changed to songsCount
+//1 many_imports  all imports changeds to java.util.*
+//2 bad_name Scanner s changed to myScanner
+//3 bad_name  inputNumber changed to songsCount
+//4 replace_condition_with_polymorphism changed toString conditions to polymorphism
+//5 replace_condition_with_polymorphism  changed distinct conditions to polymorphism
+//6 easier_to_read_code change for i in distinct to for each
+//7 long_method  method_extract getSongFromUser from main
 import java.util.*;
 
 public class Second {
@@ -10,15 +14,10 @@ public class Second {
         Scanner myScanner = new Scanner(System.in);
         int songsCount = myScanner.nextInt();
         Song[] songs = new Song[songsCount];
-        for (int i = 0; i < songs.length; i++) {
-            Song song = new Song();
-            song.songType = SongType.valueOf(myScanner.next().toUpperCase());
-            song.name = myScanner.next();
-            song.artist = myScanner.next();
-            song.lengthInSeconds = myScanner.nextInt();
-            song.numberOfDownloads = myScanner.nextInt();
-            songs[i] = song;
-        }
+
+        for (int i = 0; i < songs.length; i++)
+            songs[i] = Song.getSongFromUser(myScanner);
+
         Song.distinct(songs);
         System.out.println(Arrays.toString(Song.popSongs.toArray()));
         System.out.println(Arrays.toString(Song.rockSongs.toArray()));
@@ -39,7 +38,24 @@ class Song {
     public int numberOfDownloads;
     public int lengthInSeconds;
     public Date date;
-    public SongType songType;
+
+    public static Song getSongFromUser(Scanner myScanner){
+      Song song;
+
+      String songType = myScanner.next().toUpperCase();
+      if(songType.equals("POP")) song = new POP();
+      else if(songType.equals("ROCK")) song = new ROCK();
+      else if(songType.equals("HIPHOP")) song = new HIPHOP();
+      else if(songType.equals("JAZ")) song = new JAZ();
+      else throw new RuntimeException("undefined class");
+
+      song.name = myScanner.next();
+      song.artist = myScanner.next();
+      song.lengthInSeconds = myScanner.nextInt();
+      song.numberOfDownloads = myScanner.nextInt();
+      
+      return song;
+    }
 
     //DON'T Change This method
     public static void reset() {
@@ -49,61 +65,81 @@ class Song {
         jazSongs = new ArrayList<>();
     }
 
-    public static void distinct(Song[] newSongs) {
-        for (int i = 0; i < newSongs.length; i++) {
-            newSongs[i].date = new Date();
-            switch (newSongs[i].songType) {
-                case POP:
-                    newSongs[i].numberOfDownloads += 10;
-                    newSongs[i].artist += "Aliakbari.com";
-                    newSongs[i].name += "Sadegh";
-                    if (newSongs[i].lengthInSeconds > 300)
-                        newSongs[i].artist += "Professional";
-                    popSongs.add(newSongs[i]);
-                    break;
-                case ROCK:
-                    rockSongs.add(newSongs[i]);
-                    newSongs[i].numberOfDownloads += 10;
-                    newSongs[i].artist = "Aliakbari.com";
-                    break;
-                case HIPHOP:
-                    hiphopSongs.add(newSongs[i]);
-                    newSongs[i].numberOfDownloads += 10;
-                    break;
-                case JAZ:
-                    newSongs[i].name += "@jaz";
-                    newSongs[i].numberOfDownloads += 10;
-                    if (newSongs[i].lengthInSeconds < 180) {
-                        newSongs[i].numberOfDownloads += 5;
-                        newSongs[i].artist += "Beginner";
-                    }
-                    jazSongs.add(newSongs[i]);
+    protected void songDistinct(){
+      date = new Date();
+      numberOfDownloads += 10;
+    }
 
-            }
-        }
+    public static void distinct(Song[] newSongs) {
+        for (int i = 0; i < newSongs.length; i++)
+            newSongs[i].songDistinct();
     }
 
     @Override
     public String toString() {
-        String type = null;
-        switch (songType) {
-            case POP:
-                type = "(pop)";
-                break;
-            case ROCK:
-                type = "(rock)";
-                break;
-            case HIPHOP:
-                type = "(hiphop)";
-                break;
-            case JAZ:
-                type = "(jaz)";
-        }
-        return type + "[" + name + "  " + artist + "  " + lengthInSeconds + "  " + numberOfDownloads + "]";
+        return "[" + name + "  " + artist + "  " + lengthInSeconds + "  " + numberOfDownloads + "]";
     }
 
 }
+class POP extends Song{
+  @Override
+  protected void songDistinct(){
+      super.songDistinct();
+      numberOfDownloads += 10;
+      artist += "Aliakbari.com";
+      name += "Sadegh";
+      if (lengthInSeconds > 300)
+        artist += "Professional";
+    popSongs.add(this);
 
-enum SongType {
-    POP, ROCK, HIPHOP, JAZ
+  }
+  @Override
+  public String toString(){
+    return "(pop)"+super.toString();
+  }
+}
+
+class ROCK extends Song{
+  @Override
+  protected void songDistinct(){
+    super.songDistinct();
+    artist = "Aliakbari.com";
+    rockSongs.add(this);
+  }
+  @Override
+  public String toString(){
+    return "(rock)"+super.toString();
+  }
+}
+
+class HIPHOP extends Song{
+  @Override
+  protected void songDistinct(){
+    super.songDistinct();
+    hiphopSongs.add(this);
+  }
+  @Override
+  public String toString(){
+    return "(hiphop)"+super.toString();
+  }
+}
+
+
+class JAZ extends Song{
+
+  @Override
+  protected void songDistinct(){
+      super.songDistinct();
+      name += "@jaz";
+      if (lengthInSeconds < 180) {
+            numberOfDownloads += 5;
+            artist += "Beginner";
+      }
+      jazSongs.add(this);
+  }
+
+  @Override
+  public String toString(){
+    return "(jaz)"+super.toString();
+  }
 }
