7c42cd64b5731af0ff6cb0d986f8f6c684d61230
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-26 22:05:04 +0430

    added infoextractor basic file

---
+++ t6/OPP/InfoExtractor.java
@@ -0,0 +1,23 @@
+public class InfoExtractor{
+  int max;
+  public static void setMax(int max){
+    this.max = max;
+  }
+
+  public static List<Integer> getPaymentsPerHour(List<Person> people){
+
+  }
+
+  public static Person getLaziest(List<Person> people){
+
+  }
+
+  public static List<Employee> sortOnSuccess(List<Person> people){
+
+  }
+
+  public static List<Person> getRichestSorted(List<Person> people){
+
+  }
+
+}
