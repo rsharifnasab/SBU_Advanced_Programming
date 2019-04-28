9981a95514ef5ecdfe05903ca8ea6b0eae825776
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-28 22:45:12 +0430

    added info extractor tesst

---
+++ t6/bime2/InfoExtractorTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class InfoExtractorTest {
+
+    @Test
+    public void getPaymentsPerHour() {
+    }
+
+    @Test
+    public void getLaziest() {
+    }
+
+    @Test
+    public void sortOnSuccess() {
+    }
+
+    @Test
+    public void getRichestSorted() {
+    }
+}
