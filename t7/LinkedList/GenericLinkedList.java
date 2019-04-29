14faf1ba18fba056954b920d9cab79d0f272c7e4
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-04-29 23:19:39 +0430

    added linked list basic files

---
+++ t7/LinkedList/GenericLinkedList.java
@@ -0,0 +1,66 @@
+import java.util.Comparator;
+
+class Node<E> {
+  Node<E> next;
+}
+
+public class GenericLinkedList<T> {
+    private Node<T> first = null;
+
+    public int size() {
+       // TODO
+    }
+
+    public boolean isEmpty() {
+        // TODO
+    }
+
+    public boolean contains(Object element) {
+        // TODO
+    }
+
+    public T[] toArray() {
+        // TODO
+    }
+
+    public void add(T element) {
+        // TODO
+    }
+
+    public void add(int index, T element) {
+        // TODO
+    }
+
+
+    public boolean remove(Object element) {
+        // TODO
+    }
+
+	public boolean removeIndex(int index) {
+        // TODO
+    }
+
+    public void clear() {
+        // TODO
+    }
+
+    public T get(int index) {
+        // TODO
+    }
+
+    public void set(int index, T element) {
+        // TODO
+    }
+
+    public int indexOf(Object element) {
+        // TODO
+    }
+
+    public int lastIndexOf(Object element) {
+        // TODO
+    }
+
+    public void sort(Comparator<T> comparator) {
+        // TODO
+    }
+}
---
+++ t7/LinkedList/Test.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Test {
+    public static void main(String[] args) {
+        GenericLinkedList<Integer> list = new GenericLinkedList<>();
+        for (int i = 0; i < 3; i++)
+            list.add(i);
+        System.out.println(list.contains(1)); // true
+        System.out.println(list.get(2)); // 2
+        System.out.println(list.remove(1.2)); // false
+        System.out.println(list.remove(0)); // true
+        list.clear();
+
+        for (int i = 10; i >= 1; i--) {
+            list.add(10 - i, i);
+        }
+        System.out.println(list.indexOf(10)); // 0
+        System.out.println(list.indexOf(1)); // 9
+        System.out.println(Arrays.toString(list.toArray())); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        System.out.println(list.remove(4)); // true
+        System.out.println(Arrays.toString(list.toArray())); // [10, 9, 8, 7, 6, 5, 3, 2, 1]
+        list.set(0, 9);
+        System.out.println(Arrays.toString(list.toArray())); // [9, 9, 8, 7, 6, 5, 3, 2, 1]
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        });
+        System.out.println(Arrays.toString(list.toArray())); // [1, 2, 3, 5, 6, 7, 8, 9, 9]
+        System.out.println(list.lastIndexOf(9)); // 8
+        System.out.println(list.indexOf(9)); // 7
+        System.out.println(list.size()); // 9
+        try {
+            System.out.println(list.get(9));
+            System.out.println("Unable to catch IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Catch IndexOutOfBoundsException");
+        }
+    }
+}
