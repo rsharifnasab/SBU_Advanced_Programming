2097f1ccf53c60505ef88999ad12de5c5822c1e0
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-06-03 21:34:40 +0430

    added basic Init in reflection

--- t8/reflection/Initializer.java
+++ t8/reflection/Initializer.java
@@ -6,9 +6,26 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 public class Initializer {
-	
+
 	public static Collection<Object> init(List<String> classNames) {
-		// TODO
+		try {
+				List<Object> ans = new LinkedList<>();
+				for (String className : classNames ) {
+					Class cls = Class.forName(className);
+					System.out.println("class name:" + cls.getSimpleName());
+					System.out.println(cls.getAnnotations());
+				}
+		} catch(Exception e) {
+			throw new InitializeException();
+		}
+		finally{
+				return ans;
+		}
 	}
-	
+	public static void main(String[] args) {
+		Collection<Object> objects = Initializer.init(
+            Arrays.asList("user.Manager", "user.Service", "user.Developer"));
+
+	}
+
 }
