47c8498dcc576602b80de04ba1d06987e73b306b
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-06-05 16:09:18 +0430

    added full version, removed main from my code solved problem

--- t8/reflection/Initializer.java
+++ t8/reflection/Initializer.java
@@ -8,108 +8,51 @@ import java.util.*;
 
 public class Initializer {
 
-	public static Collection<Object> init(List<String> classNames){
-		Collection<Object> collection = new LinkedHashSet<>();
-		for (String name : classNames){
-			try {
-				Class c = Class.forName(name);
-				for(Object an : c.getDeclaredAnnotations() ){ //TODO
-					if (an instanceof Instantiate){
-						Constructor constructor = c.getDeclaredConstructor();
-						collection.add(constructor.newInstance());
-						break;
-					}
-				}
-			} catch(Exception e) {
-					throw new InitializeException();
-			}
-		}
-		for (Object obj : collection){
-			try{
-				Field[] fields = obj.getClass().getDeclaredFields();
-				for(Field field : fields){
-					for(Object annotation : field.getDeclaredAnnotations()){ //TODO
-						if(annotation instanceof Connect){
-							field.setAccessible(true);
-							boolean exists = false;
-							for(Object instance : collection){
-								if(field.getType().equals(instance.getClass())){
-									field.set(obj,instance);
-									exists = true;
-									break;
-								}
-							}
-							field.setAccessible(false);
-							if(!exists){
-								throw new InitializeException();
-							}
-						}
-					}
-				}
-			}
-			catch(Exception e){
-				throw new InitializeException();
-			}
-		}
-		return collection;
-	}
-		//-----------
-/*
+	public static Collection<Object> init(List<String> classNames1){
+		List<Object> ans = new LinkedList<>();
+		List<String>  classNames = classNames1.stream().distinct().collect(java.util.stream.Collectors.toList());
 		try {
 				for (String className : classNames ) {
 					Class cls = Class.forName(className);
+				//	System.err.println("class name: " + cls.getSimpleName());
 					for (Object a : cls.getDeclaredAnnotations() ) {
-						if(a instanceof Instantiate) {
+						if(a.toString().equals("@annotation.Instantiate()")) {
 							Object testObject = cls.getConstructor().newInstance();
+							//System.out.println("adding.. "+testObject);
 							ans.add( testObject );
+
 							break;
 						}
 					}
 				}
 					//System.out.println("--------");
 				for(Object o: ans){ //every class of input
-					//List <Field> fieldsFiltered = new LinkedList<>();
+					List <Field> fieldsFiltered = new LinkedList<>();
 					Field[] fields = o.getClass().getDeclaredFields();
-					//Arrays.stream(fields).filter(a-> a.getAnnotation(Connect.class) != null ).forEach(a->fieldsFiltered.add(a));
-					//fieldsFiltered.stream().forEach(a->a.setAccessible(true));
+					Arrays.stream(fields).filter(a-> a.getAnnotation(Connect.class) != null ).forEach(a->fieldsFiltered.add(a));
+					fieldsFiltered.stream().forEach(a->a.setAccessible(true));
 					//System.err.println("going to foreach in fileds");
-					for(Field f :  fields){
-						for(Object anot : f.getDeclaredAnnotations() ){
-							if (anot instanceof Connect == false) continue;
-							f.setAccessible(true);
-							int tedad = 0;
-							for(Object o2 : ans){
-								if(f.getType().equals(o2.getClass()) == false) continue;
-								f.set(o,o2);
-								tedad++;
-								break;
-							}
-							f.setAccessible(false);
-							if(tedad == 0) throw new InitializeException();
+					for(Field f :  fieldsFiltered){
+						Class fieldClass = f.getType();
+						Object newins = null;
+						for(Object o2:ans){
+							if (o2.getClass().equals(fieldClass) ) newins = o2;
 						}
-					}
-				}
-						//Class fieldClass = f.getType();
-						//Object newins = null;
-						//for(Object o2:ans){
-						//	if (o2.getClass().equals(fieldClass) ) newins = o2;
-						//}
 						//System.out.println("filed class type:"+fieldClass.getSimpleName());
 
-						//if (newins == null ) throw new InitializeException();
+						if (newins == null ) throw new InitializeException();
 						//System.err.println("new instance complaete");
-						//f.set(o,newins);
-
+						f.set(o,newins);
+					}
 					//System.out.println("main: " + o.getClass().getSimpleName());
 					//System.out.println("connects to: " + fieldsFiltered);
-
+				}
 
 		} catch(Exception e) {
-			e.printStackTrace();
 			throw new InitializeException();
 		}
 		return ans;
+	
 	}
-		*/
 
 }
