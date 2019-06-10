dd785c10cbae6631f98af8c39d51771d4390e0d7
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-06-10 23:37:02 +0430

    added fibo naghes , added zip of seri 1

---
+++ tamrin_ezafe/1/Fibo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Fibo {
+// compile error!
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n  = sc.nextInt() ,n2 = sc.nextInt();
+      ShowFibNth(n,n2);
+  }
+}
